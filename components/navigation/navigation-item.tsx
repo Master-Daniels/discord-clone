387c28bad566e22e8c14fbf40362dfda31d2eceb
Master-Daniels <adebayooluwasegun011@gmail.com>
2023-11-07 19:42:22 +0000

    fixed missing param

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -11,7 +11,7 @@ interface NavigationItemProps {
     name: string;
 }
 export default function NavigationItem({ id, name, imageUrl }: NavigationItemProps) {
-    const { serverId } = useParams();
+    const params = useParams();
     const router = useRouter();
 
     return (
@@ -25,13 +25,15 @@ export default function NavigationItem({ id, name, imageUrl }: NavigationItemPro
                 <div
                     className={cn(
                         "absolute left-0 bg-emerald-500 dark:bg-white rounded-r-full transition-all w-[6px]",
-                        serverId === id ? "h-[36px] bg-black dark:bg-emerald-500" : "h-[10px] group-hover:h-[36px]"
+                        params?.serverId === id
+                            ? "h-[36px] bg-black dark:bg-emerald-500"
+                            : "h-[10px] group-hover:h-[36px]"
                     )}
                 />
                 <div
                     className={cn(
                         "relative group flex h-[48px] w-[48px] mx-4 rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-                        serverId === id && "bg-primary/10 text-primary rounded-[16px]"
+                        params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
                     )}
                 >
                     <Image fill sizes="100vw" src={imageUrl} alt={name} />
