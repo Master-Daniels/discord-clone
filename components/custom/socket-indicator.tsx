1764b1d658c368c61cab2df4cb48acd80e663257
Master-Daniels <adebayooluwasegun011@gmail.com>
2023-11-07 22:35:35 +0000

    asthetic changes

--- components/custom/socket-indicator.tsx
+++ components/custom/socket-indicator.tsx
@@ -9,7 +9,7 @@ export const SocketIndicator = () => {
     if (!isConnected) {
         return (
             <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-                Fallback: poliing every second
+                Fallback<span className="hidden sm:block">: poliing every second</span>
             </Badge>
         );
     }
@@ -21,7 +21,7 @@ export const SocketIndicator = () => {
     return (
         // <div onClick={disconnect} className="cursor-pointer">
         <Badge variant="outline" className="bg-emerald-600 text-white border-none">
-            Live: Real time updates
+            Live<span className="hidden sm:block">: Real time updates</span>
         </Badge>
         // </div>
     );
